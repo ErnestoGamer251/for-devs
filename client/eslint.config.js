6e8fd262d234e5ca428d7d810731940f6cc4c762
Mateo <mateogabrieldominguez@gmail.com>
2024-10-30 15:53:34 -0300

    crear y correr el server

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,15 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.eslintrc.cjs'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -35,4 +35,4 @@ export default [
       ],
     },
   },
-]
+];
