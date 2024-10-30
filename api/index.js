ac8757cbec6c161f1aa7f69a121e83ce7d78613f
Mateo <mateogabrieldominguez@gmail.com>
2024-10-30 16:41:04 -0300

    conectado a la database

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log('MongoDB is connected');
   })
   .catch((err) => {
     console.log(err);
